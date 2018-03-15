6e08225117002a24f1fd90ad7a8b5dcbb7007c76
philipyoo <philip.yoo@scality.com>
2018-03-15 10:15:18 -0700

    [squash] fix queueprocessor double consumption

--- extensions/replication/queueProcessor/QueueProcessor.js
+++ extensions/replication/queueProcessor/QueueProcessor.js
@@ -260,8 +260,14 @@ class QueueProcessor extends EventEmitter {
 
                 this._consumer.on('metrics', data => {
                     // i.e. data = { my-bucket: { ops: 1, bytes: 124 } }
-                    this._mProducer.publishMetrics(data, metricsTypeProcessed,
-                        metricsExtension, err => {
+                    const filteredData = Object.keys(data).filter(key =>
+                        key === this.site).reduce((store, k) => {
+                            // eslint-disable-next-line no-param-reassign
+                            store[k] = data[this.site];
+                            return store;
+                        }, {});
+                    this._mProducer.publishMetrics(filteredData,
+                        metricsTypeProcessed, metricsExtension, err => {
                             this.logger.trace('error occurred in publishing ' +
                             'metrics', {
                                 error: err,
