1ea62fd31c13129990b3b43a990b4417521186f0
JianqinWang <jianqin.wang@scality.com>
2018-04-13 10:37:56 -0700

    ft: ingestion producer
    Co-authored-by: Lauren Spiegel <lhspiegel@gmail.com>

--- bin/queuePopulator.js
+++ bin/queuePopulator.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const async = require('async');
 const schedule = require('node-schedule');
 
@@ -10,6 +11,7 @@ const extConfigs = config.extensions;
 const qpConfig = config.queuePopulator;
 const mConfig = config.metrics;
 const rConfig = config.redis;
+const ingestionConfig = config.ingestion;
 const QueuePopulator = require('../lib/queuePopulator/QueuePopulator');
 
 const log = new werelogs.Logger('Backbeat:QueuePopulator');
@@ -18,18 +20,18 @@ werelogs.configure({ level: config.log.logLevel,
     dump: config.log.dumpLevel });
 
 /* eslint-disable no-param-reassign */
-function queueBatch(queuePopulator, taskState) {
+function queueBatch(queuePopulator, taskState, qpConfig, log) {
     if (taskState.batchInProgress) {
-        log.warn('skipping replication batch: previous one still in progress');
+        log.warn('skipping batch: previous one still in progress');
         return undefined;
     }
-    log.debug('start queueing replication batch');
+    log.debug('start queueing batch');
     taskState.batchInProgress = true;
     const maxRead = qpConfig.batchMaxRead;
     queuePopulator.processAllLogEntries({ maxRead }, (err, counters) => {
         taskState.batchInProgress = false;
         if (err) {
-            log.error('an error occurred during replication', {
+            log.error('an error occurred during populating', {
                 method: 'QueuePopulator::task.queueBatch',
                 error: err,
             });
@@ -38,16 +40,15 @@ function queueBatch(queuePopulator, taskState) {
         const logFunc = (counters.some(counter =>
             Object.keys(counter.queuedEntries).length > 0) ?
             log.info : log.debug).bind(log);
-        logFunc('replication batch finished', { counters });
+        logFunc('population batch finished', { counters });
         return undefined;
     });
     return undefined;
 }
 /* eslint-enable no-param-reassign */
 
-const queuePopulator = new QueuePopulator(zkConfig, kafkaConfig,
-    qpConfig, mConfig,
-    rConfig, extConfigs);
+const queuePopulator = new QueuePopulator(zkConfig, kafkaConfig, qpConfig,
+    mConfig, rConfig, extConfigs, ingestionConfig);
 
 async.waterfall([
     done => queuePopulator.open(done),
@@ -55,8 +56,15 @@ async.waterfall([
         const taskState = {
             batchInProgress: false,
         };
-        schedule.scheduleJob(qpConfig.cronRule, () => {
-            queueBatch(queuePopulator, taskState);
+        // cron rule has to change if ingestion
+        let cronRule = qpConfig.cronRule;
+        if (process.argv[2] === 'ingestion') {
+            cronRule = ingestionConfig.sources[process.argv[2]].cronRule ?
+                ingestionConfig.sources[process.argv[2]].cronRule :
+                ingestionConfig.cronRule;
+        }
+        schedule.scheduleJob(cronRule, () => {
+            queueBatch(queuePopulator, taskState, qpConfig, log);
         });
         done();
     },
